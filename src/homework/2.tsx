c5dd7329bd8a13edd827b13d14af03d14ede5126
userzahar <110730909+userzahar@users.noreply.github.com>
2023-09-12 13:21:29 +0300

    add 2 ts

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,14 @@
 import React, {useReducer} from "react";
 
+type State ={
+  isRequestInProgress: boolean,
+  requestStep: string,
+}
+
+type Action ={
+    type:'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST';
+}
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
